c157e86057d20269c677e23f3d902b9f9f63dea0
santos <santos.basnet23@gmail.com>
2016-11-25 10:31:07 +0545

    Add files via upload

---
+++ arithmatic.c
@@ -0,0 +1,29 @@
+/* santosh jung basnet
+program to do some manetnatical operation*/
+
+# include<stdio.h>
+# include<conio.h>
+
+int main()
+{printf("*******-----**********welcome*******-------**********
+        -");
+    int a,b,sum,sub,mul,div,mod;
+    printf("enter the no.s");
+    scanf("%d%d",&a,&b);
+    sum=a+b;
+    sub=a-b;
+    mul=a*b;
+    div=a/b;
+    mod=a%b;
+    printf("the sum is:%d",sum);
+    printf("the sub is :%d",sub);
+    printf("the product is:%d",mul);
+    printf("the divided answer is");
+    printf("the reminder is:%d",mod);
+    printf("-**********-------thank you santosh jung ------**************-");
+    getch();
+    return 0;
+
+
+
+}
