11a755430e1678db190d0633e895bba71ce4d39b
santos <santos.basnet23@gmail.com>
2016-11-25 10:32:18 +0545

    Add files via upload

---
+++ negative or positive.c
@@ -0,0 +1,17 @@
+/*santosh jung basnet
+program to chek whether the enter no. is positive or negative
+*/
+
+# include<stdio.h>
+# include<conio.h>
+int main()
+{
+
+    int a;
+    printf("enter the no.");
+    scanf("%d",&a);
+    printf("%d",a>0);
+    return 0;
+
+
+}
