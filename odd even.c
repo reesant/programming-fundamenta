97c30a44bad500d383956ec8895932a733d2b480
santos <santos.basnet23@gmail.com>
2016-11-25 10:51:30 +0545

    Add files via upload

---
+++ odd even.c
@@ -0,0 +1,25 @@
+/* santosh jung basnet*/
+
+# include<stdio.h>
+int main()
+{
+    int a;
+    printf("enter the no.");
+    scanf("%d",&a);
+    if (a%2==0)
+    {
+        printf("the no. is even man");
+    }
+    else
+    {
+        printf("the no is odd");
+
+
+
+    }
+    return 0;
+
+
+
+
+}
