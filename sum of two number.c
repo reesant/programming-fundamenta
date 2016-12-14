bb36c73e7ac59f8450a5fe0387e7d2bc69c0c627
santos <santos.basnet23@gmail.com>
2016-12-14 09:58:43 +0545

    Add files via upload

---
+++ sum of two number.c
@@ -0,0 +1,21 @@
+/*
+Student Name:Prabin Karki
+Subject:Programming Fundamental
+Roll No:
+Lab sheet No:3
+program:Wap to print sum of two number
+Date:16 Nov,2016
+*/
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ float a,b,sum;
+ printf("Enter the first number");
+ scanf("%f",&a);
+ printf("Enter the second number");
+ scanf("%f",&b);
+ sum=a+b;
+ printf("sum of two number is :%f",sum);
+ getch();
+}
